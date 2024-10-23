c1b55383b0f2dddf9ad49cc8bd3798e03e29e5bd
misakichi-nu <misakiksw.1110@gmail.com>
2024-10-23 23:44:01 +0900

    戻す

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="" type="image/svg+xml" />
-        <title>OAH Architecture footer</title>
+        <title>OAH Architecture</title>
         <meta name='description' content='私たちメディアアシストは、教育/研修向けの映像制作やその配信のご相談を通じて皆さまの事業の支援を行う会社として誕生しました。' />
       </Head>
       <Component {...pageProps} />
