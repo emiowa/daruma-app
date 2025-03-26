83a6ca1e50458a77d3a6975f49244ef2640e5b79
misakichi-nu <misakiksw.1110@gmail.com>
2025-03-26 19:41:17 +0900

    CSS_check

---
+++ components/links/LinkPaginasHeader.js
@@ -0,0 +1,7 @@
+import React from 'react';
+const LinkPaginasHeader = () => {
+  return (
+    <button className='border rounded border-black p-10'>Inicio</button>
+  );
+};
+export default LinkPaginasHeader;
--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Layout from "../components/Layout"
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import LinkPaginasHeader from '@/components/links/LinkPaginasHeader';
 const Home = () => {
   return (
     <div>
       <Layout>
-        <h1 className='text-red-500'>Welcome to the homepage</h1>
+        <h1 className='text-red-500 border'>Welcome toooo the homepage</h1>
+        <LinkPaginasHeader />
       </Layout>
     </div>
   );
