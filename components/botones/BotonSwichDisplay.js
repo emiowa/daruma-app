f5de0a3b4669d0a85b3f0c791e76d87f628074b6
misakichi-nu <misakiksw.1110@gmail.com>
2025-08-04 23:13:34 +0900

    fix_botonswitchdisplay

--- components/botones/BotonSwichDisplay.js
+++ components/botones/BotonSwichDisplay.js
@@ -7,11 +7,9 @@ const BotonSwichDisplay = ({ booleanItem, func, className, defaultText, changedT
   return (
     <>
       <div>
-        {booleanItem ?
-          <button onClick={() => func()} className={`p-1 md:p-2 lg:p-3 content shadow-small md:text-[13px] lg:text-[18px] ${className}`}>{defaultText}</button>
-          :
-          <button onClick={() => func()} className={`p-1 md:p-2 lg:p-3 content shadow-small md:text-[13px] lg:text-[18px] ${className}`}>{changedText}</button>
-        }
+        <button onClick={() => func()} className={`p-1 md:p-2 lg:p-3 content shadow-small md:text-[13px] lg:text-[18px] ${className}`}>
+          {booleanItem ? defaultText : changedText}
+        </button>
       </div>
     </>
   );
