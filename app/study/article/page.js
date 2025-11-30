61d1ee1311d17ca577d450fe6ef729bda1804c45
misakichi-nu <misakiksw.1110@gmail.com>
2025-11-30 23:58:49 +0900

    add_func_articleFilter

--- app/study/article/page.js
+++ app/study/article/page.js
@@ -2,8 +2,11 @@
 
 import ArticleCards from '@/components/study/ArticleCards';
 import Head from 'next/head';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import CardData from "@/data/cards.json"
+import { SlArrowDown } from "react-icons/sl";
+import { SlArrowUp } from "react-icons/sl";
+
 
 function Articles() {
 
@@ -13,7 +16,76 @@ function Articles() {
     setArticleNum(prev => prev + ArticleNum)
   }
 
+  const [currentCardsList, setCurrentCardsList] = useState(CardData)
+  const [isFilterOpen, setIsFilterOpen] = useState(false)
+  const handleClickFilterToggle = () => {
+    setIsFilterOpen(prev => !prev)
+  }
+  const filterDetail = [
+    { id: "0", title: "Por tema", list: ["Cultura", "Viajes", "Comida"] },
+    { id: "1", title: "Por nivel de dificultad", list: ["Dificultad baja", "Dificultad media", "Dificultad alta"] },
+    { id: "2", title: "Estado", list: ["Leido", "No leido"] }
+  ];
+
+  const [selectedList, setSelectedList] = useState(
+    filterDetail.map(item => ({
+      id: item.id,
+      list: []
+    }))
+  );
+
+  const toggleListItem = (id, value) => {
+    setSelectedList(prev =>
+      prev.map(item => {
+        if (item.id !== id) return item;
+        const exists = item.list.includes(value);
+        return {
+          ...item,
+          list: exists
+            ? item.list.filter(v => v !== value) // すでにある → 削除
+            : [...item.list, value]              // ない → 追加
+        };
+      })
+    );
+  };
+
+  const handleClickFilterItem = (id, value) => {
+    toggleListItem(id, value)
+  }
+  const Filter = () => {
+    return (
+
+      <div className='absolute top-8 -left-5 w-[430px] h-[250px] bg-gray-300 z-40 py-3 px-5'>
+        {filterDetail.map((item) => {
+          return (
+            <div key={item.title} className='pb-5 text-[15px]'>
+              <div className='font-bold'>{item.title}:</div>
+              <div className='flex mt-1'>
+                {item.list.map((i) => {
+                  return (
+                    <div
+                      key={i}
+                      className={`mr-4 last:mr-0 border rounded-full border-black py-1 px-2
+                        ${selectedList[item.id].list.includes(i)
+                          ? "bg-gray-600 text-white"
+                          : "bg-gray-100"
+                        }`}
+                      onClick={() => { handleClickFilterItem(item.id, i) }}
+                    >
+                      {i}
+                    </div>
+                  )
+                })}
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+
   return (
+
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -27,14 +99,25 @@ function Articles() {
             <div>き</div>
             <div>じ</div>
           </div>
+          <div className='flex absolute -top-8 left-36'>
+            <div>Filter</div>
+            {isFilterOpen ?
+              <SlArrowUp onClick={() => handleClickFilterToggle()} className='ml-6 mt-1' />
+              :
+              <SlArrowDown onClick={() => handleClickFilterToggle()} className='ml-6 mt-1' />
+            }
+            {isFilterOpen &&
+              <Filter />
+            }
+          </div>
           <div className='flex flex-wrap gap-3 mt-3 lg:mt-12 justify-center'>
             {
-              CardData.slice(0, articleNum).map((item) => (
+              currentCardsList.slice(0, articleNum).map((item) => (
                 <ArticleCards key={item.id} title={item.title} titleRuby={item.titleRuby} id={item.id} label={item.label} imageUrl={item.imageUrl} star={item.star} checked={item.checked} />
               ))
             }
           </div>
-          {CardData.length > articleNum &&
+          {currentCardsList.length > articleNum &&
             <div className='flex justify-center relative' onClick={() => handleArticleNum()}>
               <div className='md:m-9 md:w-12 md:h-12 border border-solid border-black rounded-full'>
                 <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-black text-2xl">
